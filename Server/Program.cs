8851976f5c0b99c5e9f0a40e11df1987c73830e4
jdecuyper <jerome.decuyper@gmail.com>
2016-10-12 13:40:08 -0500

    Process command line arg for server (port)

--- Server/Program.cs
+++ Server/Program.cs
@@ -6,11 +6,24 @@ namespace SocketServer
     {
         static void Main(string[] args)
         {
-            SimpleSocketServer server = new SimpleSocketServer(7000);
-            server.Start();
-            Console.WriteLine("Press any key to close server");
-            Console.ReadLine();
-            server.Stop();
+            int port = 6000;
+
+            Console.WriteLine("usage: SocketServer [port]\n");
+
+            if (args.Length == 1)
+            {
+                int portArg = 0;
+                if (int.TryParse(args[0], out portArg))
+                    port = portArg;
+
+            }
+
+            using (SimpleSocketServer server = new SimpleSocketServer(port))
+            {
+                server.Start();
+                Console.ReadLine();
+            }
+
         }
     }
 }
