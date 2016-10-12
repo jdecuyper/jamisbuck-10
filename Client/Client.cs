8c827ae57195ff6f30bac1483fdb8ef790042695
jdecuyper <jerome.decuyper@gmail.com>
2016-10-12 13:38:20 -0500

    Have client send length of query
    Before sending the query, client sends the length of the query to the server.

--- Client/Client.cs
+++ Client/Client.cs
@@ -1,7 +1,6 @@
 ﻿using System;
 using System.Net.Sockets;
 using System.Text;
-using System.Threading;
 
 namespace SocketClient
 {
@@ -10,19 +9,21 @@ namespace SocketClient
         private TcpClient _client;
         private NetworkStream _stream;
         private string _response;
-        private const int PORT_NO = 7000;
-        private const string SERVER_IP = "127.0.0.1";
-
-        public Client()
-        {
-            _client = new TcpClient(SERVER_IP, PORT_NO);
-            _stream = _client.GetStream();
-        }
 
         public Client(string host, int port)
         {
-            _client = new TcpClient(host, port);
-            _stream = _client.GetStream();
+            try
+            {
+                _client = new TcpClient(host, port);
+            }
+            catch (SocketException se)
+            {
+                Console.WriteLine(String.Format("Could not connect to host {0} on port {1}", host, port));
+                Console.Write(se.ToString() + "\n\n");
+            }
+
+            if (_client != null)
+                _stream = _client.GetStream();
         }
 
         public string ServerResponse
@@ -30,40 +31,65 @@ namespace SocketClient
             get { return _response; }
         }
 
+        public bool IsReady
+        {
+            get { return _client != null; }
+        }
+
         /// <summary>
-        /// Send number to server and receive as response a binary string.
+        /// Send integer to server and receive its binary string representation
+        /// as response.
+        ///
+        /// Not for self on how to convert a 32 bits integer into a 4-byte word:
+        /// Use BitConverter.GetBytes to convert an integer 256 into a 4-byte array.
+        ///
+        /// Example 1:
+        /// The integer 256 is converted into following byte array: [0,1,0,0] which is the
+        /// same as [0000000, 10000000, 0000000, 0000000]
+        /// If we reverse the array then we get the binary string representation
+        /// of 256: 100000000
+        ///
+        /// Example 2:
+        /// The integer 34 becomes: [34,0,0,0] => [100010, 0000000, 0000000, 0000000]
+        /// Reverse the array and we get: 100010
         /// </summary>
-        /// <param name="query">Number to convert to a binary string</param>
+        /// <param name="number">Integer that will sent to the server</param>
         public void Query(int number)
         {
-            while (!_stream.DataAvailable)
-            {
-                // Waiting for data from server
-                Thread.Sleep(5000);
-            }
-
-            // Read first response from server (should be ready\n)
-            byte[] bytes = new byte[_client.ReceiveBufferSize];
-            int bytesRead = _stream.Read(bytes, 0, _client.ReceiveBufferSize);
+            // Receive 6 bytes from server
+            byte[] bytes = new byte[6];
+            int bytesRead = _stream.Read(bytes, 0, 6);
             string ready = Encoding.ASCII.GetString(bytes, 0, 6);
 
             if (ready != "ready\n")
                 return;
 
-            // Send query 
-            // Send number
-            bytes = ASCIIEncoding.ASCII.GetBytes(number.ToString());
-            _stream.Write(bytes, 0, bytes.Length);
+            // Send length of the query as a 4 byte-word
+            string query = number.ToString();
+            int length = query.Length;
+            byte[] lengthBytes = BitConverter.GetBytes(length);
+            _stream.Write(lengthBytes, 0, 4);
+
+            // Send query
+            var queryBytes = Encoding.ASCII.GetBytes(query);
+            _stream.Write(queryBytes, 0, queryBytes.Length);
+
+            // Receive length of the response as a 4 byte-word
+            byte[] buffer = new byte[4];
+            _stream.Read(buffer, 0, 4);
+            int respLength = BitConverter.ToInt32(buffer, 0);
+            Console.WriteLine("Lengh of response: {0}", respLength);
 
             // Receive response
-            bytes = new byte[_client.ReceiveBufferSize];
-            bytesRead = _stream.Read(bytes, 0, _client.ReceiveBufferSize);
-            _response = Encoding.ASCII.GetString(bytes, 0, bytesRead);
+            byte[] responseBytes = new byte[respLength];
+            _stream.Read(responseBytes, 0, respLength);
+            _response = Encoding.ASCII.GetString(responseBytes, 0, respLength);
         }
 
         public void Dispose()
         {
-            _client.Close();
+            if (IsReady)
+                _client.Close();
         }
     }
 }
